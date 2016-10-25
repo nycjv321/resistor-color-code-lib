767bea52e529ccb388f122c08768f55147b621ba
Javier L. Velasquez <nycjv321@gmail.com>
2016-10-24 20:30:49 -0400

    changed struct to more appropriate name

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-type Color struct {
+type Band struct {
 	Name       string
 	Digit      int
 	Multiplier float64
@@ -46,19 +46,19 @@ var metricPrefixs = []MetricPrefix{MetricPrefix{Name: "yotta", ShortScale: "sept
 	MetricPrefix{Name: "zepto", ShortScale: "sextillionth", Symbol: "z", Decimal: math.Pow(10, -21)},
 	MetricPrefix{Name: "yocto", ShortScale: "septillionth", Symbol: "y", Decimal: math.Pow(10, -24)}, MetricPrefix{}}
 
-var colors = []Color{Color{Name: "Black", Digit: 0, Multiplier: 1},
-	Color{Name: "Brown", Digit: 1, Multiplier: 10, Tolerance: .01},
-	Color{Name: "Red", Digit: 2, Multiplier: math.Pow(10, 2), Tolerance: .02},
-	Color{Name: "Orange", Digit: 3, Multiplier: math.Pow(10, 3), Tolerance: .03},
-	Color{Name: "Yellow", Digit: 4, Multiplier: math.Pow(10, 4), Tolerance: .04},
-	Color{Name: "Green", Digit: 5, Multiplier: math.Pow(10, 5), Tolerance: .005},
-	Color{Name: "Blue", Digit: 6, Multiplier: math.Pow(10, 6), Tolerance: .0025},
-	Color{Name: "Violet", Digit: 7, Multiplier: math.Pow(10, 7), Tolerance: .001},
-	Color{Name: "Gray", Digit: 8, Multiplier: math.Pow(10, 8), Tolerance: .0005},
-	Color{Name: "White", Digit: 9, Multiplier: math.Pow(10, 9)},
-	Color{Name: "Gold", Multiplier: math.Pow(10, -1), Tolerance: .05},
-	Color{Name: "Silver", Multiplier: math.Pow(10, -2), Tolerance: .1},
-	Color{Tolerance: .2}}
+var Bands = []Band{Band{Name: "Black", Digit: 0, Multiplier: 1},
+	Band{Name: "Brown", Digit: 1, Multiplier: 10, Tolerance: .01},
+	Band{Name: "Red", Digit: 2, Multiplier: math.Pow(10, 2), Tolerance: .02},
+	Band{Name: "Orange", Digit: 3, Multiplier: math.Pow(10, 3), Tolerance: .03},
+	Band{Name: "Yellow", Digit: 4, Multiplier: math.Pow(10, 4), Tolerance: .04},
+	Band{Name: "Green", Digit: 5, Multiplier: math.Pow(10, 5), Tolerance: .005},
+	Band{Name: "Blue", Digit: 6, Multiplier: math.Pow(10, 6), Tolerance: .0025},
+	Band{Name: "Violet", Digit: 7, Multiplier: math.Pow(10, 7), Tolerance: .001},
+	Band{Name: "Gray", Digit: 8, Multiplier: math.Pow(10, 8), Tolerance: .0005},
+	Band{Name: "White", Digit: 9, Multiplier: math.Pow(10, 9)},
+	Band{Name: "Gold", Multiplier: math.Pow(10, -1), Tolerance: .05},
+	Band{Name: "Silver", Multiplier: math.Pow(10, -2), Tolerance: .1},
+	Band{Tolerance: .2}}
 
 func ToMetric(number float64) string {
 	prefixes := metricPrefixs
@@ -76,36 +76,36 @@ func ToMetric(number float64) string {
 	return ""
 }
 
-func findColor(color string) Color {
-	for _, element := range colors {
-		if strings.EqualFold(element.Name, strings.Trim(color, " ")) {
+func findBand(Band string) Band {
+	for _, element := range Bands {
+		if strings.EqualFold(element.Name, strings.Trim(Band, " ")) {
 			return element
 		}
 	}
-	return colors[len(colors)-1]
+	return Bands[len(Bands)-1]
 }
 
-func extractMultipler(bands []Color) ([]Color, Color) {
+func extractMultipler(bands []Band) ([]Band, Band) {
 	length := len(bands)
 	if length > 2 && length < 5 {
 		multipler, digits := bands[length-1], bands[:length-1]
 		return digits, multipler
 	} else {
-		return []Color{}, Color{}
+		return []Band{}, Band{}
 	}
 }
 
-func extractToleranceBand(bands []Color) ([]Color, Color) {
+func extractToleranceBand(bands []Band) ([]Band, Band) {
 	length := len(bands)
 	if length > 3 && length < 6 {
 		tolerance, digits := bands[length-1], bands[:length-1]
 		return digits, tolerance
 	} else {
-		return []Color{}, Color{}
+		return []Band{}, Band{}
 	}
 }
 
-func extractDigits(bands []Color) int {
+func extractDigits(bands []Band) int {
 	var buffer bytes.Buffer
 	for _, element := range bands {
 		buffer.WriteString(strconv.Itoa(element.Digit))
@@ -118,35 +118,35 @@ func extractDigits(bands []Color) int {
 	}
 }
 
-func findColors(strings []string) []Color {
-	var colors = make([]Color, len(strings), len(strings))
+func findBands(strings []string) []Band {
+	var Bands = make([]Band, len(strings), len(strings))
 	for index, element := range strings {
-		color := findColor(element)
-		if color.Name == "" {
-			fmt.Fprintf(os.Stderr, "\"%v\" was not a valid color.\n", element)
+		Band := findBand(element)
+		if Band.Name == "" {
+			fmt.Fprintf(os.Stderr, "\"%v\" was not a valid Band.\n", element)
 			os.Exit(1)
 		} else {
-			colors[index] = color
+			Bands[index] = Band
 		}
 	}
-	return colors
+	return Bands
 }
 
 func parseFlags() string {
 	var rawBands string
-	flag.StringVar(&rawBands, "bands", "Yellow,Violet,Orange,Gold", "A comma delimited list of colors representing resistor bands")
+	flag.StringVar(&rawBands, "bands", "Yellow,Violet,Orange,Gold", "A comma delimited list of Bands representing resistor bands")
 	flag.Parse()
 	return rawBands
 }
 
-func CalculateResistance(bands []Color) string {
+func CalculateResistance(bands []Band) string {
 
-	digits, toleranceColor := extractToleranceBand(bands)
-	var multiplerColor Color
-	digits, multiplerColor = extractMultipler(digits)
+	digits, toleranceBand := extractToleranceBand(bands)
+	var multiplerBand Band
+	digits, multiplerBand = extractMultipler(digits)
 
-	tolerance := toleranceColor.Tolerance * 100
-	multipler := multiplerColor.Multiplier
+	tolerance := toleranceBand.Tolerance * 100
+	multipler := multiplerBand.Multiplier
 
 	resistance := ToMetric(float64(extractDigits(digits)) * multipler)
 	if tolerance == 0 {
@@ -159,5 +159,5 @@ func CalculateResistance(bands []Color) string {
 }
 
 func main() {
-	fmt.Println(CalculateResistance(findColors(strings.Split(parseFlags(), ","))))
+	fmt.Println(CalculateResistance(findBands(strings.Split(parseFlags(), ","))))
 }
